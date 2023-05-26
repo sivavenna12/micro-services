e6a9e6b852638251b39550d7b8dc2963dc1959db
Fahath Shaik <129723395+FahathShaik@users.noreply.github.com>
2023-05-26 16:13:12 +0530

    new-change (#39)

--- admin-service/src/main/java/com/feuji/adminservice/controller/ExamController.java
+++ admin-service/src/main/java/com/feuji/adminservice/controller/ExamController.java
@@ -1,17 +1,22 @@
 package com.feuji.adminservice.controller;
 
-import com.feuji.adminservice.service.ExamService;
-import com.feuji.commonmodel.Exam;
-import com.feuji.commonmodel.Question;
-import com.feuji.commonmodel.Subject;
+import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Set;
+import com.feuji.adminservice.service.ExamService;
+import com.feuji.commonmodel.Exam;
+import com.feuji.commonmodel.Subject;
 
 @RestController
 @CrossOrigin("*")
